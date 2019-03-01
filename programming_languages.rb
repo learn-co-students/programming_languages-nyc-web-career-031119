cbbe5fe69452fbd048c477d8170c48bbd162afaa
danielmartin0126 <danielmartin0126@gmail.com>
2019-03-01 04:08:21 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,80 @@
+require "pry"
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style_type,language_list|
+      language_list.each do |lang, type|
+        if new_hash.has_key?(lang)
+          new_hash[lang][:style] << style_type
+        else
+          new_hash[lang] = type
+          new_hash[lang][:style] = [style_type]
+      end
+      end
+    end
+
+
+  new_hash
 end
+reformat_languages(languages)
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
